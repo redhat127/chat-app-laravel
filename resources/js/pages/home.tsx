0bd37d5a73d04397a85bd55dc476a56f2542a021
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-11-30 13:51:08 +0330

    enhance social login buttons with GitHub and Google icons for better visual appeal

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,6 +1,8 @@
 import { BaseLayout } from '@/components/layout/base';
 import { Button } from '@/components/ui/button';
 import { useUser } from '@/hooks/use-user';
+import { GithubIcon } from '@/icons/github';
+import { GoogleIcon } from '@/icons/google';
 import { generateTitle } from '@/lib/utils';
 import login from '@/routes/login';
 import { Head } from '@inertiajs/react';
@@ -17,10 +19,16 @@ export default function Home() {
         <div className="flex min-h-screen items-center justify-center">
           <div className="mx-auto flex max-w-xl flex-col gap-2 p-4">
             <Button asChild>
-              <a href={login.provider.redirect.url({ provider: 'github' })}>Login with Github</a>
+              <a href={login.provider.redirect.url({ provider: 'github' })}>
+                <GithubIcon />
+                Continue with Github
+              </a>
             </Button>
             <Button asChild variant="orange">
-              <a href={login.provider.redirect.url({ provider: 'google' })}>Login with Google</a>
+              <a href={login.provider.redirect.url({ provider: 'google' })}>
+                <GoogleIcon />
+                Continue with Google
+              </a>
             </Button>
           </div>
         </div>
