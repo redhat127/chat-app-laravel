f4aaa082351b4e5055af9f64ebda05bcd58505c0
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-01 08:41:39 +0330

    configure DatabaseSeeder to seed users table with one user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,22 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void {}
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'saeid',
+            'email' => 'saeid@example.com',
+            'password' => Hash::make('saeid123456'),
+        ]);
+    }
 }
