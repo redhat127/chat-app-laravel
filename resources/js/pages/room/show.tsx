18ab4ba038daba1c4dc2c09ca07a1795cd3966cb
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-03 21:26:52 +0330

    show online users count in each room

--- resources/js/pages/room/show.tsx
+++ resources/js/pages/room/show.tsx
@@ -4,14 +4,14 @@ import { BaseLayout } from '@/components/layout/base';
 import { MessageList } from '@/components/message/message-list';
 import { InviteUserModal } from '@/components/room/invite-users-dialog';
 import { LeaveRoomForm } from '@/components/room/leave-room-form';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { generateTitle } from '@/lib/utils';
 import { home } from '@/routes';
 import type { Message, Room } from '@/types';
 import { Head, Link } from '@inertiajs/react';
-import { useEcho } from '@laravel/echo-react';
+import { echo, useEcho } from '@laravel/echo-react';
 import { useQueryClient } from '@tanstack/react-query';
 import { CircleAlert, Ellipsis, UserPlus } from 'lucide-react';
 import { useEffect, useState, type ReactNode } from 'react';
@@ -26,16 +26,49 @@ export default function ShowRoom({
   currentUserIsCreator: boolean;
 }) {
   const queryClient = useQueryClient();
-  const { listen, leave } = useEcho<{ new_message: Message }>('room.' + room.id, 'BroadcastMessageEvent', (data) => {
+
+  const [roomOnlineUserIds, setRoomOnlineUserIds] = useState<string[]>([]);
+
+  const { listen: roomListen, leave: roomLeave } = useEcho<{ new_message: Message }>('room.' + room.id, 'BroadcastMessageEvent', (data) => {
     queryClient.setQueryData<Message[]>(['messages', { roomId: room.id }], (messages = []) => {
       return [...messages, data.new_message];
     });
   });
 
   useEffect(() => {
-    listen();
-    return () => leave();
-  }, [leave, listen]);
+    roomListen();
+    return () => {
+      roomLeave();
+    };
+  }, [roomListen, roomLeave]);
+
+  const e = echo();
+  const roomOnlineUserIdsChannel = e.join('room.' + room.id + '.user-id');
+
+  useEffect(() => {
+    roomOnlineUserIdsChannel.subscribe();
+
+    roomOnlineUserIdsChannel.here((users: Array<{ user_id: string; current_user_exists_as_member: boolean }>) => {
+      setRoomOnlineUserIds(users.filter((user) => user.current_user_exists_as_member).map((user) => user.user_id));
+    });
+
+    roomOnlineUserIdsChannel.joining((data: { user_id: string; current_user_exists_as_member: boolean }) => {
+      if (!data.current_user_exists_as_member) return;
+      setRoomOnlineUserIds((prev) => {
+        if (prev.includes(data.user_id)) return prev;
+        return [...prev, data.user_id];
+      });
+    });
+
+    roomOnlineUserIdsChannel.leaving((data: { user_id: string; current_user_exists_as_member: boolean }) => {
+      if (!data.current_user_exists_as_member) return;
+      setRoomOnlineUserIds((prev) => {
+        return prev.filter((uId) => uId !== data.user_id);
+      });
+    });
+
+    return () => roomOnlineUserIdsChannel.unsubscribe();
+  }, [roomOnlineUserIdsChannel]);
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -45,6 +78,12 @@ export default function ShowRoom({
       <Head>
         <title>{generateTitle(room.name)}</title>
       </Head>
+      <Card className="fixed top-4 left-1/2 -translate-x-1/2 py-2 text-sm">
+        <CardContent className="flex items-center gap-1.5 px-4">
+          <div className="size-1.5 animate-pulse rounded-full bg-green-600" />
+          {roomOnlineUserIds.length} {roomOnlineUserIds.length === 1 ? 'Person' : 'People'} Online
+        </CardContent>
+      </Card>
       <div className="mb-48 space-y-4 p-4 px-8">
         <Card className="rounded-md py-2">
           <CardHeader className="flex items-center justify-between gap-4">
--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,7 @@
 
 use App\Models\ChatRoom;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\DB;
 
 Broadcast::channel('room.{roomId}', function ($user, string $roomId) {
     $room = ChatRoom::query()->public()->find($roomId);
@@ -11,4 +12,16 @@ Broadcast::channel('room.{roomId}', function ($user, string $roomId) {
     }
 
     return true;
+});
+
+Broadcast::channel('room.{roomId}.user-id', function ($user, string $roomId) {
+    $currentUserExistsAsMember = DB::table('chat_room_member')
+        ->where('chat_room_id', $roomId)
+        ->where('member_id', $user->id)
+        ->exists();
+
+    return [
+        'user_id' => $user->id,
+        'current_user_exists_as_member' => $currentUserExistsAsMember,
+    ];
 });
